0710f2135f04fc7f44562caed2c6a06ccf61353b
inayat hussain <inayat.rdec@gmail.com>
2016-09-24 14:20:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -30,9 +30,11 @@ app.get('/ui/a.png', function (req, res) {
 app.get('/article1', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
+
 app.get('/article2', function (req, res) {
   res.send("Article 2 is responded");
 });
+
 app.get('/article3', function (req, res) {
   res.send("Article 3 is responded");
 });
