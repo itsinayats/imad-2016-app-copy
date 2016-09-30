a64e81a1d8c566cee5df3ad5d03ec4b8803dd73e
inayat hussain <inayat hussain>
2016-09-30 21:32:09 +0530

    client side changes

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
 console.log('Loaded!');
+alert("welcome");
+
+//show address onclick
+function show_address(){
+var element=document.getElementById('address');
+element.innerHTML= '<br>c1-20,Pusa ,Samastipur,Bihar(848125)';
+}
+
+//MOve MY PROFILE PIC
+var img= document.getElementById('imgi');
+img.onclick = function() {
+img.style.marginLeft='100px';
+};
