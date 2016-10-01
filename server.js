1a0a976012874ac65c289317b4e4da8182adc241
inayat hussain <inayat.rdec@gmail.com>
2016-10-01 18:55:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -47,13 +47,22 @@ res.sendFile(path.join(__dirname, 'ui/images' , 'fb.jpg'))
 app.get('/ui/images/gl.jpg',function(req,res){
 res.sendFile(path.join(__dirname, 'ui/images' , 'gl.jpg'))
 });
-/*
+
+var counter=0;
+app.get('/counter',function(req, res){
+counter = counter + 1;
+res.send(counter.toString());
+});
+
+
+
+
+
 app.get('/:articleName', function (req, res) {
  var articleName=req.params.articleName;
 res.send(createTemplate(articles[articleName]));
  });
 
-*/
  
  
 /* CREATING TEMPLATE*/
@@ -188,11 +197,6 @@ return template;
 
 
 
-var counter=0;
-app.get('/counter',function(req, res){
-counter = counter + 1;
-res.send(counter.toString());
-});
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
