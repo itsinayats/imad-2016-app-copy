0d286186c70aa5f158205712985d494c7f07fa7e
inayat hussain <inayat.rdec@gmail.com>
2016-10-01 17:57:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -53,9 +53,10 @@ app.get('/:articleName', function (req, res) {
 res.send(createTemplate(articles[articleName]));
  });
 
+
 var counter=0;
-app.get('/counter',function(req,res){
-counter=counter+1;
+app.get('/counter',function(req, res){
+counter = counter + 1;
 res.send(counter.toString());
 });
  
