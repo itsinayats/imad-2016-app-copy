ded75eded9d3ddac20fa258ec65975878988f9d2
inayat hussain <inayat.rdec@gmail.com>
2016-10-28 10:57:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.get('/', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/ui/login.php', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
