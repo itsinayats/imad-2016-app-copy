68ec0de979f9e13fe6fc519457a6b91316697f25
inayat hussain <inayat.rdec@gmail.com>
2016-11-05 16:12:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,6 +26,16 @@ res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 
 var pool=new pool(config);
 app.get('/test-db', function (req, res) {
+    pool.querry('select * from user',function(err,res){
+        if(err)
+        {
+          res.status(500).send(err.toString()) ;
+        }
+        else
+        {
+            
+        }
+    });
  
 });
 
