71c297e9d2f88653c4db8da66a6995a706655900
inayat hussain <inayat.rdec@gmail.com>
2016-11-11 00:39:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -329,9 +329,9 @@ var template= `
     </div>
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#">Login</a></li>
-           <li><a href="#">Register</a></li>
+            <li class="active bold"><a href="#">Home</a></li>
+            <li><a href="#" class="bold">Login</a></li>
+           <li><a href="#" class="bold">Register</a></li>
            </ul>
            <div class="col-sm-3 col-md-3 pull-right">
             <form class="navbar-form" role="search">
