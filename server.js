7208a097a6fca6f11e77633087cddf0802689d2c
inayat hussain <inayat.rdec@gmail.com>
2016-11-11 01:02:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -298,8 +298,7 @@ var template= `
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <link rel="stylesheet" href="ui/style.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ 
   		<style>
   .affix {
       top: 0;
@@ -416,7 +415,9 @@ var template= `
 <footer class="container-fluid">
   <p>Footer Text</p>
 </footer>
-
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="/ui/main.js"></script>
 </body>
 </html>
 
