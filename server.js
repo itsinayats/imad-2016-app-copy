c18da1e9d8df3e89cb79ba5daa7b7c7af7f7c477
inayat hussain <inayat.rdec@gmail.com>
2016-11-11 02:31:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -354,20 +354,16 @@ var template= `
    <div class="container-fluid">
 	<div class="row">
 	
-	<!--left-->
-	<div class="navbar-header">
+	  <!--left-->
       <div class="col-md-3" id="leftCol">
-      
-        <ul class="nav nav-stacked ">
-         <h3>CATEGORIES</h3>
-          <li><a href="#sec0">Section A</a></li>
+        <ul class="nav nav-stacked" id="sidebar">
+          <li><a href="#sec0">Section 0</a></li>
           <li><a href="#sec1">Section 1</a></li>
           <li><a href="#sec2">Section 2</a></li>
           <li><a href="#sec3">Section 3</a></li>
           <li><a href="#sec4">Section 4</a></li>
         </ul>
-    </div>
-    </div><!--/left-->
+      </div><!--/left-->
     
       <div class="col-sm-9">
     
