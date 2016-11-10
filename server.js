3c6974136cda066361dad83ce7a70563fa6048e1
inayat hussain <inayat.rdec@gmail.com>
2016-11-11 03:19:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -361,7 +361,7 @@ var template= `
       <div class="col-md-3" id="leftCol">
       <ul class="nav nav-stacked affix s" style="position:fixed">
       <br>  <br><br>
-      <button class="affix btn btn-lg" data-toggle="collapse" data-target="#cat" style="width:300px;text-align:center;top:50px">Categories</button>
+      <button class=" btn btn-lg" data-toggle="collapse" data-target="#cat" style="width:300px;text-align:center;">Categories</button>
       <div id="cat" class="collapse">
           <li><a href="#sec0">Section 0</a></li>
           <li><a href="#sec1">Section 1</a></li>
