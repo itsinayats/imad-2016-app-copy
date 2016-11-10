ad715bc92288f5fbbeb72e1191ce4d5c3466661c
inayat hussain <inayat.rdec@gmail.com>
2016-11-11 00:43:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -324,7 +324,7 @@ var template= `
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-       <a href="#" class="navbar-brand bold active">Inayat's Blog</a>
+       <a href="#" class="navbar-brand active">Inayat's Blog</a>
        
     </div>
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
