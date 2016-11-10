5fc1b492e61c490392314ac3adbe544875dad6ad
inayat hussain <inayat.rdec@gmail.com>
2016-11-11 01:40:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -375,7 +375,7 @@ var template= `
       <div class="col-md-2" id="leftCol">
       <h3>CATEGORIES</h3>
         <ul class="nav nav-stacked" id="sidebar">
-          <li><a href="#sec0">Section 0</a></li>
+          <li><a href="#sec0">Section A</a></li>
           <li><a href="#sec1">Section 1</a></li>
           <li><a href="#sec2">Section 2</a></li>
           <li><a href="#sec3">Section 3</a></li>
