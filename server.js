776c6bf49b54c29066a5e2bb964899d08f23292f
inayat hussain <inayat.rdec@gmail.com>
2016-11-11 00:02:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -300,7 +300,16 @@ var template= `
   <link rel="stylesheet" href="ui/style.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  
+  		<style>
+  .affix {
+      top: 0;
+      width: 100%;
+  }
+
+ .affix + .container-fluid {
+      padding-top: 70px;
+  }
+</style>
 </head>
 <body>
  <!--NAVBAR STARTS  -->
