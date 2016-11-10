1734ce1ec03f6b45c061d9e7cbe7545976d24857
inayat hussain <inayat.rdec@gmail.com>
2016-11-11 00:37:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -331,9 +331,18 @@ var template= `
         <ul class="nav navbar-nav">
             <li class="active"><a href="#">Link</a></li>
             <li><a href="#">Link</a></li>
-            <li>lllll</li>
-        </ul>
-                
+            <li></li>
+           </ul>
+           <div class="col-sm-3 col-md-3 pull-right">
+            <form class="navbar-form" role="search">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search" name="q">
+                    <div class="input-group-btn">
+                        <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                    </div>
+                </div>
+            </form>
+        </div>        
     </div>
 </nav>
   <!--NAVBAR ENDS  -->
