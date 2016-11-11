4f4852a4042a8d4b5197a906897ffb79ea512281
inayat hussain <inayat.rdec@gmail.com>
2016-11-11 13:10:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -167,7 +167,7 @@ res.send(JSON.stringify(names));
 app.get('/articles/:articleName', function (req, res) {
 // var articleName=req.params.articleName;
 //pool.query("Select * from articles,users where title='" + req.params.articleName + "'", function(err,result){
-pool.query("select article_tags.tag,articles.title,articles.content,articles.category,articles.heading, articles,date,users.name from articles,article_tags,users where article_tags.article_id=articles.id AND articles.title='"+ req.params.articleName +"' AND articles.author_id=users.id;",function(err,result)){
+pool.query("select article_tags.tag,articles.title,articles.content,articles.category,articles.heading, articles,date,users.name from articles,article_tags,users where article_tags.article_id=articles.id AND articles.title='"+ req.params.articleName +"' AND articles.author_id=users.id;",function(err,result){
     
     
     
