96a2e5acfba83b28b0169dea5d58e332bf427bce
inayat hussain <inayat.rdec@gmail.com>
2016-11-11 14:10:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -360,11 +360,7 @@ return template;
 
 
 
-
-http.listen(process.env.PORT || 3000, function(){
-  console.log('listening on', http.address().port);
-
-//var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-//app.listen(8080, function () {
-//  console.log(`IMAD course app listening on port ${port}!`);
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
+ console.log(`IMAD course app listening on port ${port}!`);
 });
